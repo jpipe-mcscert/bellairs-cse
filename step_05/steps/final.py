57d0a570258544bda695df76d769d156c116d21a
Sébastien Mosser <sebastien.mosser@gmail.com>
2026-03-02 11:10:52 -0400

    step 5

--- step_05/steps/final.py
+++ step_05/steps/final.py
@@ -5,10 +5,10 @@
 from typing import Any, Callable
 from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
 
-# jpipe-runner --library steps/deployable.py --diagram deployable --format svg deployable.json
+# jpipe-runner --library steps/final.py --diagram final --format svg final.json
 
 
-# git checkout -b step_04
+# git checkout -b step_05
 # touch modified_file.py
 # git add modified_file.py 
 # git commit -m "modified file added"  
@@ -16,8 +16,8 @@ from jpipe_runner.framework.decorators.jpipe_decorator import jpipe
 
 # Cleanup
 # git checkout main
-# git push -d origin step_04
-# git branch -D step_04
+# git push -d origin step_05
+# git branch -D step_05
 
 mock = {
     'accuracy':     0.82,
@@ -26,18 +26,10 @@ mock = {
     'counterfacts': 'counterfacts.csv'
 }
 
-#################
-#### Shared #####
-#################
 
-## Evidence e1
-@jpipe(produce=["model"])
-def model_is_available(produce: Callable[[str, Any], None]) -> bool:
-    # Check that the model file exists and can be loaded
-    if 'model_file' in mock:
-        produce('model', True)
-        return True
-    return False
+@jpipe()
+def all_conditions_are_met():
+    return True
 
 ######################
 #### Performance #####
@@ -49,15 +41,15 @@ def my_model_is_performant(produce: Callable[[str, Any], None]) -> bool:
     # nothing more to do than reaching this stage
     return True
 
-## Strategy s
 @jpipe(consume=["model", "d"], produce=["a"])
-def accuracy_is_greater_than_85(model: bool, d: bool,
+def accuracy_is_greater_than_80(model: bool, d: bool,
                                 produce: Callable[[str, Any], None]) -> bool:
     # load the model; load the tests; measure accuracy
-    if model and d and mock['accuracy'] > 0.85:
+    if model and d and mock['accuracy'] > 0.80:
         produce("a", mock['accuracy'])
         return True
     return False
+
 
 @jpipe(produce=["d"])
 def test_dataset_is_available(produce: Callable[[str, Any], None]) -> bool:
@@ -90,9 +82,25 @@ def counterfactual_dataset_is_available(produce: Callable[[str, Any], None]) -> 
         return True
     return False
 
+#####################
+#### Convergence ####
+#####################
 
+## Strategy check_convergence
+@jpipe(consume=["logs", "model"])
+def assessing_logs_to_measure_convergence(logs: bool, model: bool) -> bool:
+    return logs and model
 
+## Evidence e2_conv
+@jpipe(produce=["logs"])
+def intermediate_logs_are_available(produce: Callable[[str, Any], None]) -> bool:
+    produce('logs', True)
+    return True
 
-
+## Evidence e1_conv
+@jpipe(produce=["model"])
+def trained_model_is_available(produce: Callable[[str, Any], None]) -> bool:
+    produce('model', True)
+    return True
 
 
